16e13b77616ddce615bcefd9858c3d07f0b45658
Anastasia Savina <savnastya26@yandex.ru>
2024-01-11 00:07:21 +0300

    feat: made routes

---
+++ routes/users.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const { getUsers, getUserById, updateUser, updateAvatar, createUsers } = require("../controllers/users");
+
+router.get('/', getUsers);
+router.get('/:userId', getUserById);
+router.post('/', createUsers);
+router.patch('/users/me', updateUser);
+router.patch('/users/me/avatar', updateAvatar);
+
+module.exports = router;

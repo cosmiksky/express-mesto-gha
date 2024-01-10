15b331603f31b1ccb5b68fd8e965324b54193426
Anastasia Savina <savnastya26@yandex.ru>
2024-01-11 02:01:54 +0300

    fix err

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const { getUsers, getUserById, updateUser, updateAvatar, createUsers } = require('../controllers/users');
 
-router.get('/', getUsers);
-router.get('/:userId', getUserById);
-router.post('/', createUsers);
+router.get('/users', getUsers);
+router.get('/users/:userId', getUserById);
+router.post('/users', createUsers);
 router.patch('/users/me', updateUser);
 router.patch('/users/me/avatar', updateAvatar);
 

c7841c4b376558a330c467231c15d7a9a9bbb452
Anastasia Savina <savnastya26@yandex.ru>
2024-01-11 01:41:49 +0300

    change port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const { PORT = 5000 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 app.use(bodyParser.json());

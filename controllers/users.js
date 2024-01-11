9f6c35e67465e3c0701a86949f3a7b8beb5d6946
Anastasia Savina <savnastya26@yandex.ru>
2024-01-11 03:03:27 +0300

    fix update

--- controllers/users.js
+++ controllers/users.js
@@ -20,21 +20,24 @@ module.exports.getUserById = (req, res) => {
 module.exports.createUsers = (req, res) => {
 	const { name, about, avatar } = req.body;
 	User.create({ name, about, avatar })
-		.then( user => res.send({data: user}))
+		.then( user => res.send(user))
 		.catch(() => res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя' }));
 };
 
 module.exports.updateUser = (req, res) => {
 	const { name, about } = req.body;
 	User.findByIdAndUpdate(req.params.id, { name, about })
-		.then(user => res.status(200).send({data: user})
-		)
+		.then( user => {
+			if(!user) {
+				res.status(404).send({ message: 'Пользователь не найден' });
+			}
+			res.send({data: user});})
 		.catch(() => res.status(404).send({ message: 'Пользователь не найден' }));
 };
 
 module.exports.updateAvatar = (req, res) => {
 	const { avatar } = req.body;
 	User.findByIdAndUpdate(req.params.id, { avatar })
-		.then( user => res.send({data: user}))
+		.then( user => res.send(user))
 		.catch(() => res.status(404).send({ message: 'Пользователь не найден' }));
 };

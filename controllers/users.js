e5ee304064b6c6c1ed9f6f05c5638bc66d942fee
Anastasia Savina <savnastya26@yandex.ru>
2024-01-11 23:38:43 +0300

    fix try status

--- controllers/users.js
+++ controllers/users.js
@@ -9,13 +9,12 @@ module.exports.getUsers = (req, res) => {
 module.exports.getUserById = (req, res) => {
 	const {userId}  = req.params;
 	User.findById(userId)
-		.then((user) => {
-			if (!user) {
-				res.status(404).send({ message: 'Пользователь не найден' });
-			}
-			res.send(user);
-		})
-		.catch(() => res.status(400).send({ message: 'Переданы некорректные данные пользователя' }));
+		.then((user) => res.send(user))
+		.catch((res) => {
+			if (res.status === 404) {res.send({ message: 'Пользователь не найден' });}
+
+			res.status(400).send({ message: 'Переданы некорректные данные пользователя' });
+		});
 };
 
 module.exports.createUsers = (req, res) => {

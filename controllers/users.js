0762afdef39441e51f42715c11c069dd1c9e63b2
Anastasia Savina <savnastya26@yandex.ru>
2024-01-20 00:01:41 +0300

    asd

--- controllers/users.js
+++ controllers/users.js
@@ -1,25 +1,44 @@
 const User = require('../models/users');
 
-module.exports.getUsers = (req, res) => {
+module.exports.getUsers = (req, res, next) => {
 	User.find({})
-		.then(users => res.send(users))
-		.catch(() => res.status(404).send({ message: 'Пользователь не найден' }));
+		.then(users => {
+			if(users) {
+				res.send(users);} else {
+				res.status(404).send({ message: 'Пользователь не найден' });
+			}}
+		)
+		.catch((err) => {
+			next(err);
+		});
 };
 
-module.exports.getUserById = (req, res) => {
-	const {userId}  = req.params;
+module.exports.getUserById = (req, res, next) => {
+	const userId  = req.params;
 	User.findById(userId)
-		.then((user) => res.send(user))
-		.catch(() =>
-			res.status(400).send({ message: 'Переданы некорректные данные пользователя' })
-		);
+		.then((user) => {
+			if(user) {
+				res.send(user);
+			} else {
+				throw new Error('Пользователь по переданному id не найден');
+			}
+		})
+		.catch((err) => {
+			next(err);
+		});
 };
 
-module.exports.createUsers = (req, res) => {
+module.exports.createUsers = (req, res, next) => {
 	const { name, about, avatar } = req.body;
 	User.create({ name, about, avatar })
 		.then( user => res.send(user))
-		.catch(() => res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя' }));
+		.catch((err) => {
+			if(err) {
+				res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя' });
+			} else {
+				next(err);
+			}
+		});
 };
 
 module.exports.updateUser = (req, res) => {
